eb4acd3ec27f2450e329ffbe3334a40f6eb42e05
Arash Arora <52750629+arash-arora@users.noreply.github.com>
2022-04-06 21:56:38 +0530

    contact page done

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,14 +1,9 @@
-<<<<<<< HEAD
 import ContactForm from './Form'
 import Map from './Map'
-=======
-import React from 'react'
->>>>>>> 5d53821a94f287621d69ee68cd655032d30baa63
 
 function Contact() {
   return (
     <div className="mx-24">
-<<<<<<< HEAD
       <ContactForm />
       <hr className="mt-20" />
       <div className="mt-20 flex flex-col items-center justify-between text-center md:flex-row md:text-left">
@@ -18,13 +13,6 @@ function Contact() {
           </h1>
         </div>
         <div className="flex flex-row flex-wrap gap-10 md:flex-col">
-=======
-      <div className="mt-20 flex justify-between">
-        <div>
-          <h1 className="text-4xl">Get in Touch</h1>
-        </div>
-        <div className="flex flex-col gap-10">
->>>>>>> 5d53821a94f287621d69ee68cd655032d30baa63
           <div>
             <h3 className="text-xl font-semibold">Collaborate</h3>
             <p className="text-[#999]">support@wavemakers.in</p>
@@ -36,11 +24,7 @@ function Contact() {
             <p className="text-[#999]">+91 8766212312</p>
           </div>
         </div>
-<<<<<<< HEAD
-        <div className="mt-10 flex flex-row flex-wrap gap-10 md:mt-0">
-=======
-        <div className="flex flex-col gap-10">
->>>>>>> 5d53821a94f287621d69ee68cd655032d30baa63
+        <div className="mt-10 flex flex-row flex-wrap gap-10 md:mt-0 md:flex-col">
           <div>
             <h3 className="text-xl font-semibold">Press</h3>
             <p className="text-[#999]">support@wavemakers.in</p>
@@ -54,7 +38,6 @@ function Contact() {
         </div>
       </div>
       <hr className="mt-10" />
-<<<<<<< HEAD
       <div className="mt-20 flex flex-col items-center justify-between md:flex-row">
         <div>
           <h1 className="mb-10 whitespace-nowrap text-4xl md:mb-0">
@@ -62,31 +45,20 @@ function Contact() {
           </h1>
         </div>
         <div className="flex flex-row flex-wrap gap-10 md:flex-col">
-=======
-      <div className="mt-20 flex justify-between">
-        <div>
-          <h1 className="text-4xl">Locations</h1>
-        </div>
-        <div className="flex flex-col gap-10">
->>>>>>> 5d53821a94f287621d69ee68cd655032d30baa63
           <div>
-            <h3 className="text-xl font-semibold">Delhi</h3>
+            <h3 className="text-xl font-semibold">East of Kailash</h3>
             <p className="text-[#999]">support@wavemakers.in</p>
             <p className="text-[#999]">+91 8766212312</p>
           </div>
           <div>
-            <h3 className="text-xl font-semibold">UAE</h3>
+            <h3 className="text-xl font-semibold">Bahrain</h3>
             <p className="text-[#999]">support@wavemakers.in</p>
             <p className="text-[#999]">+91 8766212312</p>
           </div>
         </div>
-<<<<<<< HEAD
-        <div className="flex flex-row flex-wrap gap-10 sm:mt-10 md:mt-0 ">
-=======
-        <div className="flex flex-col gap-10">
->>>>>>> 5d53821a94f287621d69ee68cd655032d30baa63
+        <div className="flex flex-row flex-wrap gap-10 sm:mt-10 md:mt-0 md:flex-col">
           <div>
-            <h3 className="text-xl font-semibold">London</h3>
+            <h3 className="text-xl font-semibold">Bhikaji</h3>
             <p className="text-[#999]">support@wavemakers.in</p>
             <p className="text-[#999]">+91 8766212312</p>
           </div>
@@ -97,12 +69,9 @@ function Contact() {
           </div>
         </div>
       </div>
-<<<<<<< HEAD
       <section className="my-20 mb-32 hidden h-[500px] w-full max-w-none md:block">
         <Map />
       </section>
-=======
->>>>>>> 5d53821a94f287621d69ee68cd655032d30baa63
     </div>
   )
 }
