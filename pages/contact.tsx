5d53821a94f287621d69ee68cd655032d30baa63
Arash Arora <52750629+arash-arora@users.noreply.github.com>
2022-04-06 14:35:33 +0530

    contact page started

---
+++ pages/contact.tsx
@@ -0,0 +1,16 @@
+import Contact from '../components/Contact'
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+import Banner from '../components/Banner'
+function contact() {
+  return (
+    <>
+      <Header />
+      <Banner />
+      <Contact />
+      <Footer />
+    </>
+  )
+}
+
+export default contact
