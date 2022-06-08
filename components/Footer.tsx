7897490cc90999042a63a63c6ec5ebeab68414da
Arash Arora <52750629+arash-arora@users.noreply.github.com>
2022-06-08 13:32:11 +0530

    added social links

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,8 +1,9 @@
 import logo from '../assets/images/wavemakers1.png'
 import Image from 'next/image'
 function Footer() {
+  //F9FAFB
   return (
-    <footer className="body-font mt-20 bg-[#F9FAFB] text-gray-600">
+    <footer className="body-font bg-[#fff] text-gray-600">
       <div className="container mx-auto flex flex-col flex-wrap px-5 py-24 md:flex-row md:flex-nowrap md:items-center lg:items-start">
         <div className="mx-auto w-64 flex-shrink-0 text-center md:mx-0 md:text-left">
           <a className="title-font flex items-center justify-center font-medium text-gray-900 md:justify-start">
@@ -84,7 +85,7 @@ function Footer() {
           </div>
         </div>
       </div>
-      <div className="bg-gray-100">
+      <div className="bg-white">
         <div className="container mx-auto flex flex-col flex-wrap py-4 px-5 sm:flex-row">
           <p className="text-center text-sm text-gray-500 sm:text-left">
             © 2022 Wavemakers —
@@ -98,7 +99,10 @@ function Footer() {
             </a>
           </p>
           <span className="mt-2 inline-flex justify-center sm:ml-auto sm:mt-0 sm:justify-start">
-            <a className="text-gray-500">
+            <a
+              href="https://www.facebook.com/wavemakerss"
+              className="text-gray-500"
+            >
               <svg
                 fill="currentColor"
                 stroke-linecap="round"
@@ -110,7 +114,7 @@ function Footer() {
                 <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
               </svg>
             </a>
-            <a className="ml-3 text-gray-500">
+            <a className="ml-3 text-gray-500" href="">
               <svg
                 fill="currentColor"
                 stroke-linecap="round"
@@ -122,7 +126,10 @@ function Footer() {
                 <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
               </svg>
             </a>
-            <a className="ml-3 text-gray-500">
+            <a
+              className="ml-3 text-gray-500"
+              href="https://instagram.com/_wave_makers?igshid=YmMyMTA2M2Y="
+            >
               <svg
                 fill="none"
                 stroke="currentColor"
@@ -136,7 +143,10 @@ function Footer() {
                 <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
               </svg>
             </a>
-            <a className="ml-3 text-gray-500">
+            <a
+              className="ml-3 text-gray-500"
+              href="https://www.linkedin.com/company/79148449/admin/"
+            >
               <svg
                 fill="currentColor"
                 stroke="currentColor"
